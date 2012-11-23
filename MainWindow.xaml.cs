9fb3ec486319bedecc3560048657e9b274f0e993
mikeabrahamsen <mail@michaelabrahamsen.com>
2012-11-23 08:26:38 -0900

    Added Plug and Play functionality
    - Added Reference to Microsoft.Kinect.Toolkit
    - Added Reference to Microsoft.Samples.Kinect.WpfViewers
    - We are now using the KinectSensorManager wrapper
      For an idea of how to use this look at the InitializeKinectServices
      method
    - Moved all kinect setup code to InitializeKinectServices
    - Added WindowClosing and WindowClosed methods

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,14 @@
 ﻿
+using System;
 using System.IO;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Data;
 using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Samples.Kinect.WpfViewers;
 
 namespace KineticsAnalyzer
 {
@@ -12,12 +17,15 @@ namespace KineticsAnalyzer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static readonly DependencyProperty KinectSensorManagerProperty =
+            DependencyProperty.Register(
+                "KinectSensorManager",
+                typeof(KinectSensorManager),
+                typeof(MainWindow),
+                new PropertyMetadata(null));
+
         private static readonly int Bgr32BytesPerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
 
-        /// <summary>
-        /// Kinect sensor used to capture depth data
-        /// </summary>
-        private KinectSensor kinectSensor;
 
         /// <summary>
         /// Image for drawing depth information from the kinect
@@ -35,57 +43,100 @@ namespace KineticsAnalyzer
         private byte[] colorPixels;
 
         /// <summary>
+        /// Kinect sensor chooser from the KinectToolkit
+        /// </summary>
+        
+        private readonly KinectSensorChooser sensorChooser = new KinectSensorChooser();
+
+        /// <summary>
         /// Initializes a new instance of the MainWindow class.
         /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            this.SensorChooserUI.KinectSensorChooser = sensorChooser;
+            sensorChooser.Start();
+
         }
 
+        /// <summary>
+        /// Actions to take when the window is loaded
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            this.KinectSensorManager = new KinectSensorManager();
+
+            //Subscribe to the KinectSensorChanged Event
+            this.KinectSensorManager.KinectSensorChanged += this.KinectSensorChanged;
+            this.DataContext = this.KinectSensorManager;
+
+            this.SensorChooserUI.KinectSensorChooser = sensorChooser;
+            sensorChooser.Start();
+
+            // Bind the KinectSensor from the sensorChooser to the KinectSensor on the KinectSensorManager
+            var kinectSensorBinding = new Binding("Kinect") { Source = this.sensorChooser };
+            BindingOperations.SetBinding(this.KinectSensorManager, KinectSensorManager.KinectSensorProperty, kinectSensorBinding);
+
+
+        }
+        
+        /// <summary>
+        /// Event that is triggered when the sensor is changed, this will be used to stop the old sensor and
+        /// start the new sensor.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void KinectSensorChanged(object sender, KinectSensorManagerEventArgs<KinectSensor> args)
+        {
+            if (null != args.OldValue)
             {
-                if (potentialSensor.Status == KinectStatus.Connected)
-                {
-                    kinectSensor = potentialSensor;
-                    break;
-                }
+                this.UninitializeKinectServices(args.OldValue);
             }
 
-            if (this.kinectSensor != null)
+
+            if (null != args.NewValue)
             {
-                kinectSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
-                kinectSensor.SkeletonStream.Enable();
-
-                depthImagePixels = new DepthImagePixel[kinectSensor.DepthStream.FramePixelDataLength];
-                colorPixels = new byte[kinectSensor.DepthStream.FramePixelDataLength * sizeof(int)];
-                depthImageBitmap = new WriteableBitmap(kinectSensor.DepthStream.FrameWidth,
-                    kinectSensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
-
-                DepthImage.Source = depthImageBitmap;
-                kinectSensor.DepthFrameReady += KinectSensorDepthFrameReady;
-
-                try
-                {
-                    kinectSensor.Start();
-                }
-                catch (IOException)
-                {
-                    this.kinectSensor = null;
-                }
-            }
-
-            if (kinectSensor == null)
-            {
-                statusBarText.Text = Properties.Resources.NoKinectReady;
-            }
-            else
-            {
-                statusBarText.Text = Properties.Resources.KinectReady;
+                this.InitializeKinectServices(this.KinectSensorManager, args.NewValue);
             }
         }
 
+        /// <summary>
+        /// Setup Kinect Services
+        /// </summary>
+        /// <param name="kinectSensorManager"></param>
+        /// <param name="sensor"></param>
+        private void InitializeKinectServices(KinectSensorManager kinectSensorManager, KinectSensor sensor)
+        {
+            // Enable Streams that are being used
+            kinectSensorManager.SkeletonStreamEnabled = true;
+            kinectSensorManager.DepthStreamEnabled = true;
+            
+            kinectSensorManager.DepthFormat = DepthImageFormat.Resolution640x480Fps30;
+            depthImagePixels = new DepthImagePixel[sensor.DepthStream.FramePixelDataLength];
+            colorPixels = new byte[sensor.DepthStream.FramePixelDataLength * sizeof(int)];
+            depthImageBitmap = new WriteableBitmap(sensor.DepthStream.FrameWidth,
+                sensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+            DepthImage.Source = depthImageBitmap;
+            sensor.DepthFrameReady += KinectSensorDepthFrameReady;
+
+            // Set statusbar to ready
+            statusBarText.Text = Properties.Resources.KinectReady;
+        }
+
+        /// <summary>
+        /// Uninitialize kinect services
+        /// </summary>
+        /// <param name="sensor"></param>
+        private void UninitializeKinectServices(KinectSensor sensor)
+        {
+            DepthImage.Source = null;
+            sensor.DepthFrameReady -= KinectSensorDepthFrameReady;
+            statusBarText.Text = Properties.Resources.NoKinectReady;
+        }
+        
         private void KinectSensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
         {
             int imageWidth = 0;
@@ -115,6 +166,35 @@ namespace KineticsAnalyzer
                         colorPixels, imageWidth * Bgr32BytesPerPixel, 0);
                 }
             }
+        }
+
+        /// <summary>
+        /// Get and set KinectSensorManager
+        /// </summary>
+        public KinectSensorManager KinectSensorManager
+        {
+            get { return (KinectSensorManager)GetValue(KinectSensorManagerProperty); }
+            set { SetValue(KinectSensorManagerProperty, value); }
+        }
+
+        /// <summary>
+        /// Window Closing Actions
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WindowClosing(object sender, CancelEventArgs e)
+        {
+            sensorChooser.Stop();
+        }
+
+        /// <summary>
+        /// Window Closed Actions
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WindowClosed(object sender, EventArgs e)
+        {
+            this.KinectSensorManager.KinectSensor = null;
         }
     }
 }
