0b4d6f0472fbf688892bc264b9dfea7c4044aebc
Erik S. Carlsten <ecarlste.register@gmail.com>
2012-12-11 16:58:48 -0700

    Added custom control to display skeleton analysis data

---
+++ Controls/AnalyzedKinectSkeleton.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace KineticsAnalyzer.Controls
+{
+    class AnalyzedKinectSkeleton : Control
+    {
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+        }
+    }
+}
